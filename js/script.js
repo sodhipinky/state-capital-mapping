eceeb3b4694ef66296e2c08f0d0a0e9e047d67d1
Pinky Sodhi <pinky.sodhi@niit.com>
2023-09-03 02:44:00 +0530

    added validation to enable the submit button

--- js/script.js
+++ js/script.js
@@ -12,4 +12,17 @@ function drop(event) {
         let data = event.dataTransfer.getData("text");
         event.target.appendChild(document.getElementById(data));
     }
+    let states = document.getElementById("state-row").getElementsByClassName("card");
+    let flag = 0;
+    for (let index = 0; index < states.length; index++) {
+        if (states[index].getElementsByClassName("card-body")[0].children.length > 0) {
+            flag++;
+        }
+    }
+    if (flag === states.length) {
+        document.getElementById("btn-submit").disabled = false;
+    }
+    else {
+        document.getElementById("btn-submit").disabled = true;
+    }
 }

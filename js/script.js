cd06f47a34d2ff7db212f10724ed999bb7b6434b
Pinky Sodhi <pinky.sodhi@niit.com>
2023-09-03 00:40:01 +0530

    defined the drag and drop functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function drag(event) {
     event.dataTransfer.setData("text", event.target.id);
 }
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    if (event.srcElement.children.length === 0) {
+        event.preventDefault();
+        let data = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
+    }
+}

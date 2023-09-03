e4ac7ff095b901ff2e4121e7c075a16d3a476f1b
Pinky Sodhi <pinky.sodhi@niit.com>
2023-09-03 12:58:21 +0530

    refactored the code to fix bugs

--- js/script.js
+++ js/script.js
@@ -53,48 +53,52 @@ function allowDrop(event) {
 }
 
 function drop(event) {
-    if (event.srcElement.children.length === 0) {
-        event.preventDefault();
-        let data = event.dataTransfer.getData("text");
-        event.target.appendChild(document.getElementById(data));
-    }
-    let states = document.getElementById("state-row").getElementsByClassName("card");
-    let flag = 0;
-    for (let index = 0; index < states.length; index++) {
-        if (states[index].getElementsByClassName("card-body")[0].children.length > 0) {
-            flag++;
+    if (event.srcElement.id.indexOf("btn-cap-") === -1) {
+        if (event.srcElement.children.length === 0) {
+            event.preventDefault();
+            let data = event.dataTransfer.getData("text");
+            event.target.appendChild(document.getElementById(data));
         }
-    }
-    if (flag === states.length) {
-        document.getElementById("btn-submit").disabled = false;
-    }
-    else {
-        document.getElementById("btn-submit").disabled = true;
+        let states = document.getElementById("state-row").getElementsByClassName("card");
+        let flag = 0;
+        for (let index = 0; index < states.length; index++) {
+            if (states[index].getElementsByClassName("card-body")[0].children.length > 0) {
+                flag++;
+            }
+        }
+        if (flag === states.length) {
+            document.getElementById("btn-submit").disabled = false;
+        }
+        else {
+            document.getElementById("btn-submit").disabled = true;
+        }
     }
 }
 
 function submit() {
     let states = document.getElementsByClassName("card");
-    let result = false;
-    for (let index = 0; index < states.length; index++) {
+    let result = false; let index = 0;
+    do {
         switch (states[index].children[0].innerText) {
             case "Haryana":
-                states[index].children[1].innerText === "Chandigarh" ? result = true : result = false;
+                result = states[index].children[1].innerText === "Chandigarh" ? true : false;
                 break;
             case "Uttar Pradesh":
-                states[index].children[1].innerText === "Lucknow" ? result = true : result = false;
+                result = states[index].children[1].innerText === "Lucknow" ? true : false;
                 break;
             case "Himachal":
-                states[index].children[1].innerText === "Shimla" ? result = true : result = false;
+                result = states[index].children[1].innerText === "Shimla" ? true : false;
                 break;
             case "Uttarakhand":
-                states[index].children[1].innerText === "Dehradun" ? result = true : result = false;
+                result = states[index].children[1].innerText === "Dehradun" ? true : false;
                 break;
             case "Rajasthan":
-                states[index].children[1].innerText === "Jaipur" ? result = true : result = false;
+                result = states[index].children[1].innerText === "Jaipur" ? true : false;
                 break;
         }
+        index++;
     }
+    while (result && index < states.length);
     let message = "That's Incorrect.";
     if (result) {
         message = "That's Correct."
